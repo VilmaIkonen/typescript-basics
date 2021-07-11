e499465ac0793b8724c0fea8c91ba1c009bf1049
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-07-11 10:06:56 +0300

    Basic example of interface

---
+++ 04_interfaces/interfaces.basics.ts
@@ -0,0 +1,38 @@
+// Interface descripes structure of an object (only name of the type)
+
+interface Greetable {
+	name: string
+	greet(phrase: string): void
+}
+
+interface DefineAge {
+	age: number
+	printAge(phrase: string): void
+}
+
+// Class can implement several interfaces (but can only extend on class)
+class Person implements Greetable, DefineAge {
+	name: string
+	age: number
+	city = 'Helsinki'
+
+	// properties from interfaces:
+	constructor(n: string, a: number) {
+		this.name = n
+		this.age = a
+	}
+
+	// methods from interfaces:
+	greet(phrase: string) {
+		console.log(phrase + ' ' + this.name)  // return nothing as method type = void
+	}
+
+	printAge(phrase: string) {
+		console.log(phrase + ' ' + this.age + ' years old')
+	}
+}
+
+const user1 = new Person('Matt', 30)
+user1.greet('Hi, my name is')
+user1.printAge('I am')
+console.log(user1) // --> Person {city: "Helsinki", name: "Matt", age: 30}
